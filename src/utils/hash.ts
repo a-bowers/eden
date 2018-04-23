9a219772218960c20c737c535d4330797f71ef10
Abhishek Hingnikar <abhishek@hingnikar.com>
2018-04-23 05:20:47 -0400

    Let there be Queue

---
+++ src/utils/hash.ts
@@ -0,0 +1,11 @@
+import {createHash} from 'crypto';
+
+const ALGORITHM = 'md5';
+
+export function hashJson(data: any) {
+    return hash(JSON.stringify(data));
+}
+
+export function hash(data: Buffer | string) {
+    return createHash(ALGORITHM).update(data).digest('hex');
+}
