623ec73673e5345617288943bb35441a4ed9b920
Abhishek Hingnikar <abhishek@hingnikar.com>
2018-04-28 00:46:03 -0400

    App done --- needs testing

---
+++ src/error/HttpError.ts
@@ -0,0 +1,22 @@
+import {STATUS_CODES} from 'http';
+
+export class HttpError extends Error {
+    public code: number;
+    public message: string;
+
+    constructor(code: number, message = STATUS_CODES[code]) {
+        super(`Error(${code}): ${message}`);
+        if (!STATUS_CODES[code]) {
+            throw Error("Invalid Http Error");
+        }
+        this.code = code;
+        this.message = message!;
+    }
+
+    public toJSON(){
+        return {
+            code: this.code,
+            message: this.message
+        }
+    }
+}
